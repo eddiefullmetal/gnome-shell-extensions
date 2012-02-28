d3bc6d34785396495d55808e23dcc0e3845b1e0a
Grigoris Grigoriadis <eddiefullmetal@gmail.com>
2012-02-29 00:08:28 +0200

    Added one more visibility state, Overview only. Cleaned up code.

--- panelSettings@eddiefullmetal.gr/extension.js
+++ panelSettings@eddiefullmetal.gr/extension.js
@@ -10,6 +10,141 @@ const PanelMenu = imports.ui.panelMenu;
 
 const ExtensionPath = imports.ui.extensionSystem.extensionMeta['panelSettings@eddiefullmetal.gr'].path;
 
+/* Panel Visibility States */
+
+//Visibility States
+const VISIBILITY_NORMAL = -1;
+const VISIBILITY_AUTOHIDE = 0;
+const VISIBILITY_OVERVIEW_ONLY = 1;
+
+/* Base State */
+function VisibilityBaseState(originalPanelHeight){
+    this._init(originalPanelHeight);
+}
+
+VisibilityBaseState.prototype = {
+    _init: function(originalPanelHeight){
+        this._originalPanelHeight = originalPanelHeight;
+    },
+    onPanelMouseEnter: function(){
+    },
+    onPanelMouseLeave: function(){
+    },
+    onOverviewHiding: function(){
+    },
+    onOverviewShowing: function(){
+    },
+    onMenuOpenStateChanged: function(menu, open){
+    },
+    destroy: function(){
+    },
+    _hidePanel: function(){
+        Tweener.addTween(Main.panel.actor, {
+            height: 1,
+            opacity: 255,
+            time: 0.3,
+            transition: 'easeOutQuad',
+            onUpdate: function() {
+                Main.panel.actor.set_clip(0,0,Main.panel.actor.get_width(),Main.panel.actor.get_height());
+            }
+        });
+    },
+    _showPanel: function(){
+        Tweener.addTween(Main.panel.actor, {
+            height: this._originalPanelHeight,
+            opacity: 255,
+            time: 0.3,
+            transition: 'easeOutQuad',
+            onComplete: function() {
+                Main.panel.actor.remove_clip();
+            },
+            onUpdate: function() {
+                Main.panel.actor.set_clip(0,0,Main.panel.actor.get_width(),Main.panel.actor.get_height());
+            }
+        });       
+    }
+}
+
+/* None State */
+function VisibilityNormalState(originalPanelHeight){
+    this._init(originalPanelHeight);
+}
+
+VisibilityNormalState.prototype = {
+    __proto__: VisibilityBaseState.prototype,
+    _init: function(originalPanelHeight){
+        VisibilityBaseState.prototype._init.call(this, originalPanelHeight);
+
+        this._showPanel();
+    }
+}
+
+/* Autohide State */
+function VisibilityAutohideState(originalPanelHeight){
+    this._init(originalPanelHeight);
+}
+
+VisibilityAutohideState.prototype = {
+    __proto__: VisibilityBaseState.prototype,
+    _init: function(originalPanelHeight){
+        VisibilityBaseState.prototype._init.call(this, originalPanelHeight);
+
+        if(!Main.overview.visible){
+            this._hidePanel();
+        }
+    },
+    onPanelMouseEnter: function(){
+        this._pendingHideRequest = false;
+        this._showPanel();
+    },
+    onPanelMouseLeave: function(){
+        //If the overview is visible or a menu is shown do not hide the panel
+        var canHide = !Main.overview.visible && Main.panel._menus._activeMenu == null;
+        this._pendingHideRequest = !canHide;
+        if(canHide){
+            this._hidePanel();
+        }
+    },
+    onOverviewHiding: function(){
+        if(this._pendingHideRequest){
+            this._hidePanel();
+        }
+    },
+    onMenuOpenStateChanged: function(menu, open){
+        if(!open && this._pendingHideRequest && !Main.overview.visible){
+            this._hidePanel();
+        }
+    },
+    destroy: function(){
+        this._showPanel();
+    }
+}
+
+/* Overview Only State */
+function VisibilityOverviewOnlyState(originalPanelHeight){
+    this._init(originalPanelHeight);
+}
+
+VisibilityOverviewOnlyState.prototype = {
+    __proto__: VisibilityBaseState.prototype,
+    _init: function(originalPanelHeight){
+        VisibilityBaseState.prototype._init.call(this, originalPanelHeight);
+
+        if(!Main.overview.visible){
+            this._hidePanel();
+        }
+    },
+    onOverviewHiding: function(){
+        this._hidePanel();
+    },
+    onOverviewShowing: function(){
+        this._showPanel();
+    },
+    destroy: function(){
+        this._showPanel();
+    }
+}
+
 /* Settings Manager  */
 function SettingsManager(){
     this._init();
@@ -46,18 +181,16 @@ PanelVisibilityManager.prototype = {
         this._settings = settings;
         let settings = this._settings.load();
 
-        if(settings.autohide){
-            this.isAutohide = settings.autohide;
-            if(this.isAutohide){
-                this._hidePanel();
-            }
+        if(settings.visibilityState != undefined){
+            this.setState(settings.visibilityState);
         }else{
-            this.isAutohide = false;
+            this.setState(VISIBILITY_NORMAL);
         }
 
         this._panelEnterEventId = Main.panel.actor.connect('enter-event', Lang.bind(this, this._onPanelMouseEnter));
         this._panelLeaveEventId = Main.panel.actor.connect('leave-event', Lang.bind(this, this._onPanelMouseLeave));
         this._overviewHidingEventId = Main.overview.connect('hiding', Lang.bind(this, this._onOverviewHiding));
+        this._overviewShowingEventId = Main.overview.connect('showing', Lang.bind(this, this._onOverviewShowing));
 
         //Add a hook on the addMenu so that we can connect to open-state-changed events of menus added after the extension.
         this.originalAddMenuFunc = Main.panel._menus.addMenu;
@@ -82,66 +215,43 @@ PanelVisibilityManager.prototype = {
             this._menuAdded(menu);
         }
     },
-    toggleAutohide: function(){
-        this.isAutohide = !this.isAutohide;
-        this._settings.save({autohide:this.isAutohide});
-        if(this.isAutohide){
-            this._hidePanel();
-        }else{
-            this._showPanel();
+    setState: function(visibilityState){
+        this.visibilityState = visibilityState;
+        this._settings.save({visibilityState:this.visibilityState});
+
+        if(this._visibilityStateImpl){
+            this._visibilityStateImpl.destroy();
+        }
+
+        switch(this.visibilityState){
+            case VISIBILITY_AUTOHIDE:
+                this._visibilityStateImpl = new VisibilityAutohideState(this._originalHeight);
+                break;
+            case VISIBILITY_OVERVIEW_ONLY:
+                this._visibilityStateImpl = new VisibilityOverviewOnlyState(this._originalHeight);
+                break;
+            case VISIBILITY_NORMAL:
+                this._visibilityStateImpl = new VisibilityNormalState(this._originalHeight);
+                break;
         }
     },
     _onPanelMouseEnter: function(){
-        this._pendingHideRequest = false;
-        if(this.isAutohide){
-            this._showPanel();
-        }
+        this._visibilityStateImpl.onPanelMouseEnter();
     },
     _onPanelMouseLeave: function(){
-        //If the overview is visible or a menu is shown do not hide the panel
-        var canHide = !Main.overview.visible && Main.panel._menus._activeMenu == null;
-        this._pendingHideRequest = !canHide;
-        if(this.isAutohide && canHide){
-            this._hidePanel();
-        }
+        this._visibilityStateImpl.onPanelMouseLeave();
     },
-    _onMenuOpenState: function(menu, open){
-        if(!open && this.isAutohide && this._pendingHideRequest){
-            this._hidePanel();
-        }
+    _onMenuOpenStateChanged: function(menu, open){
+        this._visibilityStateImpl.onMenuOpenStateChanged();
     },
     _onOverviewHiding: function(){
-        if(this.isAutohide && this._pendingHideRequest){
-            this._hidePanel();
-        }
+        this._visibilityStateImpl.onOverviewHiding();
     },
-    _hidePanel: function(){
-        Tweener.addTween(Main.panel.actor, {
-            height: 1,
-            opacity: 255,
-            time: 0.3,
-            transition: "easeOutQuad",
-            onUpdate: function() {
-                Main.panel.actor.set_clip(0,0,Main.panel.actor.get_width(),Main.panel.actor.get_height());
-            }
-        });
-    },
-    _showPanel: function(){
-        Tweener.addTween(Main.panel.actor, {
-            height: this._originalHeight,
-            opacity: 255,
-            time: 0.3,
-            transition: "easeOutQuad",
-            onComplete: function() {
-                Main.panel.actor.remove_clip();
-            },
-            onUpdate: function() {
-                Main.panel.actor.set_clip(0,0,Main.panel.actor.get_width(),Main.panel.actor.get_height());
-            }
-        });       
+    _onOverviewShowing: function(){
+        this._visibilityStateImpl.onOverviewShowing();
     },
     _menuAdded: function(menu){
-        let eventId = menu.connect('open-state-changed', Lang.bind(this, this._onMenuOpenState));
+        let eventId = menu.connect('open-state-changed', Lang.bind(this, this._onMenuOpenStateChanged));
         this._menuData.push({ menu: menu, eventId: eventId});
     },
     _menuRemoved: function(menu){
@@ -162,10 +272,12 @@ PanelVisibilityManager.prototype = {
         return -1;
     },
     destroy: function(){
-        this._showPanel();
+        this._visibilityStateImpl.destroy();
+        
         Main.panel.actor.disconnect(this._panelEnterEventId);
         Main.panel.actor.disconnect(this._panelLeaveEventId);
         Main.overview.disconnect(this._overviewHidingEventId);
+        Main.overview.disconnect(this._overviewShowingEventId);
 
         Main.panel._menus.addMenu = this.originalAddMenuFunc;
         Main.panel._menus.removeMenu = this.originalRemoveMenuFunc;
@@ -190,12 +302,41 @@ PanelSettingsButton.prototype = {
 
         this._settings = new SettingsManager;
         this._visibilityManager = new PanelVisibilityManager(this._settings);
+        
+        this._visibilityItems = new Array;
+    
+        this._visibilityItems[VISIBILITY_NORMAL] = new PopupMenu.PopupMenuItem("Normal");
+        this._visibilityItems[VISIBILITY_NORMAL].connect('activate', Lang.bind(this, this._onVisibilityNormalItemClick));
+        this.menu.addMenuItem(this._visibilityItems[VISIBILITY_NORMAL]);
+        
+        this._visibilityItems[VISIBILITY_AUTOHIDE] = new PopupMenu.PopupMenuItem("Autohide");
+        this._visibilityItems[VISIBILITY_AUTOHIDE].connect('activate', Lang.bind(this, this._onVisibilityAutohideItemClick));
+        this.menu.addMenuItem(this._visibilityItems[VISIBILITY_AUTOHIDE]);
 
-        this._visibilityAutohide = new PopupMenu.PopupSwitchMenuItem("Autohide");
-        this._visibilityAutohide.setToggleState(this._visibilityManager.isAutohide);
-        this.menu.addMenuItem(this._visibilityAutohide);
-
-        this._visibilityAutohide.connect('activate', Lang.bind(this._visibilityManager, this._visibilityManager.toggleAutohide));
+        this._visibilityItems[VISIBILITY_OVERVIEW_ONLY] = new PopupMenu.PopupMenuItem("Overview Only");
+        this._visibilityItems[VISIBILITY_OVERVIEW_ONLY].connect('activate', Lang.bind(this, this._onVisibilityOverviewOnlyItemClick));        
+        this.menu.addMenuItem(this._visibilityItems[VISIBILITY_OVERVIEW_ONLY]);
+        
+        this._updateVisibilityState();
+    },
+    _onVisibilityOverviewOnlyItemClick: function(){
+        this._visibilityManager.setState(VISIBILITY_OVERVIEW_ONLY);
+        this._updateVisibilityState();
+    },
+    _onVisibilityAutohideItemClick: function(){
+        this._visibilityManager.setState(VISIBILITY_AUTOHIDE);
+        this._updateVisibilityState();
+    },
+    _onVisibilityNormalItemClick: function(){
+        this._visibilityManager.setState(VISIBILITY_NORMAL);
+        this._updateVisibilityState();
+    },
+    _updateVisibilityState: function(){
+        for(let index in this._visibilityItems){
+            this._visibilityItems[index].setShowDot(false);
+        }
+        
+        this._visibilityItems[this._visibilityManager.visibilityState].setShowDot(true);
     },
     _destroy: function(){
         this._visibilityManager.destroy();
