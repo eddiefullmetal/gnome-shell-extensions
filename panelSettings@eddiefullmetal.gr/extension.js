33d12189d895b87201c9984ab8d667d06e3d6dd0
Grigoris Grigoriadis <eddiefullmetal@gmail.com>
2012-03-03 19:30:52 +0200

    Implementing panel Layout(Top,Bottom). Some issues are not yet resolved when the layout is set to bottom: 1. Hot corner doesn't work. 2. System Tray appears on top of the panel.

--- panelSettings@eddiefullmetal.gr/extension.js
+++ panelSettings@eddiefullmetal.gr/extension.js
@@ -10,12 +10,66 @@ const PanelMenu = imports.ui.panelMenu;
 
 const ExtensionPath = imports.ui.extensionSystem.extensionMeta['panelSettings@eddiefullmetal.gr'].path;
 
+/* Generic Helper Classes */
+function MenuHook(menuAddedCallback, menuRemovedCallback){
+    this._init(menuAddedCallback, menuRemovedCallback);
+}
+
+MenuHook.prototype = {
+    _init: function(menuAddedCallback, menuRemovedCallback){
+        this._originalAddMenuFunc = Main.panel._menus.addMenu;
+        this._originalRemoveMenuFunc = Main.panel._menus.removeMenu;
+        this._menuAddedCallback = menuAddedCallback;
+        this._menuRemovedCallback = menuRemovedCallback;
+
+        Main.panel._menus.addMenu =  Lang.bind(this, function(menu, position){
+            this._originalAddMenuFunc.apply(Main.panel._menus,[menu, position]);            
+            this._menuAddedCallback(menu, position);
+        });
+
+        Main.panel._menus.removeMenu = Lang.bind(this, function(menu){
+            this._originalRemoveMenuFunc.apply(Main.panel._menus,[menu]);         
+            this._menuRemovedCallback(menu);
+        });
+    },
+    destroy: function(){
+        Main.panel._menus.addMenu = this._originalAddMenuFunc;
+        Main.panel._menus.removeMenu = this._originalRemoveMenuFunc;   
+    }
+}
+
+function SettingsManager(){
+    this._init();
+}
+
+SettingsManager.prototype = {
+    _init: function(){
+        this._file = Gio.file_new_for_path(ExtensionPath + '/settings.json');
+        this.settings = new Object;
+    },
+    save: function(){
+        this._file.replace_contents(JSON.stringify(this.settings), null, false, 0, null);
+    },
+    load: function(){
+        if(this._file.query_exists(null)) {
+            [flag, data] = this._file.load_contents(null);
+
+            if(flag){
+                this.settings = JSON.parse(data);
+            }
+        }
+    }
+}
+
 /* Panel Visibility States */
 
 //Visibility States
 const VISIBILITY_NORMAL = -1;
 const VISIBILITY_AUTOHIDE = 0;
 const VISIBILITY_OVERVIEW_ONLY = 1;
+
+const LAYOUT_TOP = 0;
+const LAYOUT_BOTTOM = 1;
 
 /* Base State */
 function VisibilityBaseState(originalPanelHeight){
@@ -39,35 +93,66 @@ VisibilityBaseState.prototype = {
     destroy: function(){
     },
     _hidePanelNoAnim: function(){
-        Main.panel.actor.set_height(1);
-        Main.panel.actor.set_clip(0, 0, Main.panel.actor.get_width(), 1);
+        let actor = Main.panel.actor.get_parent();
+        
+        if(actor.get_y() == 0){
+            actor.set_height(1);
+            actor.set_clip(0, 0, actor.get_width(), 1);
+        } else {
+            let y = actor.get_y() + actor.get_height() -1;
+            actor.set_y(y);
+        }
     },
     _showPanelNoAnim: function(){
-        Main.panel.actor.set_height(this._originalPanelHeight);
-        Main.panel.actor.remove_clip();
+        let actor = Main.panel.actor.get_parent();
+        
+        if(actor.get_y() == 0){        
+            actor.set_height(this._originalPanelHeight);
+            actor.remove_clip();
+        } else {
+            actor.set_y(Main.layoutManager.primaryMonitor.height - actor.get_height());
+        }
     },
     _hidePanel: function(){
-        Tweener.addTween(Main.panel.actor, {
-            height: 1,
-            time: 0.3,
-            transition: 'easeOutQuad',
-            onUpdate: function() {
-                Main.panel.actor.set_clip(0,0,Main.panel.actor.get_width(),Main.panel.actor.get_height());
-            }
-        });
+        let actor = Main.panel.actor.get_parent();
+        if(actor.get_y() == 0){        
+            Tweener.addTween(actor, {
+                height: 1,
+                time: 0.3,
+                transition: 'easeOutQuad',
+                onUpdate: function() {
+                    actor.set_clip(0, 0, actor.get_width(), actor.get_height());
+                }
+            });
+        } else {
+            Tweener.addTween(actor, {
+                y: actor.get_y() + actor.get_height() - 1,
+                time: 0.3,
+                transition: 'easeOutQuad'
+            });
+        }
     },
     _showPanel: function(){
-        Tweener.addTween(Main.panel.actor, {
-            height: this._originalPanelHeight,
-            time: 0.3,
-            transition: 'easeOutQuad',
-            onComplete: function() {
-                Main.panel.actor.remove_clip();
-            },
-            onUpdate: function() {
-                Main.panel.actor.set_clip(0,0,Main.panel.actor.get_width(),Main.panel.actor.get_height());
-            }
-        });       
+        let actor = Main.panel.actor.get_parent();
+        if(actor.get_y() == 0){
+            Tweener.addTween(actor, {
+                height: this._originalPanelHeight,
+                time: 0.3,
+                transition: 'easeOutQuad',
+                onComplete: function() {
+                    actor.remove_clip();
+                },
+                onUpdate: function() {
+                    actor.set_clip(0, 0, actor.get_width(), actor.get_height());
+                }
+            });
+        } else {
+            Tweener.addTween(actor, {
+                y: Main.layoutManager.primaryMonitor.height - actor.get_height(),
+                time: 0.3,
+                transition: 'easeOutQuad'
+            });
+        }
     }
 }
 
@@ -149,31 +234,6 @@ VisibilityOverviewOnlyState.prototype = {
     }
 }
 
-/* Settings Manager  */
-function SettingsManager(){
-    this._init();
-}
-
-SettingsManager.prototype = {
-    _init: function(){
-        this._file = Gio.file_new_for_path(ExtensionPath + '/settings.json');
-    },
-    save: function(settings){
-        this._file.replace_contents(JSON.stringify(settings), null, false, 0, null);
-    },
-    load: function(){
-        if(this._file.query_exists(null)) {
-            [flag, data] = this._file.load_contents(null);
-
-            if(flag){
-                return JSON.parse(data);
-            }else{
-                return null;
-            }
-        }
-    }
-}
-
 /* Panel Visibility Manager */
 function PanelVisibilityManager(settings){
     this._init(settings);
@@ -183,10 +243,9 @@ PanelVisibilityManager.prototype = {
     _init: function(settings){
         this._originalHeight = Main.panel.actor.get_height();		
         this._settings = settings;
-        let settings = this._settings.load();
 
-        if(settings.visibilityState != undefined){
-            this.setState(settings.visibilityState);
+        if(this._settings.settings.visibilityState != undefined){
+            this.setState(this._settings.settings.visibilityState);
         }else{
             this.setState(VISIBILITY_NORMAL);
         }
@@ -196,21 +255,10 @@ PanelVisibilityManager.prototype = {
         this._overviewHidingEventId = Main.overview.connect('hiding', Lang.bind(this, this._onOverviewHiding));
         this._overviewShowingEventId = Main.overview.connect('showing', Lang.bind(this, this._onOverviewShowing));
 
-        //Add a hook on the addMenu so that we can connect to open-state-changed events of menus added after the extension.
-        this.originalAddMenuFunc = Main.panel._menus.addMenu;
+        let menuAddedCallback = Lang.bind(this, this._menuAdded);
+        let menuRemovedCallback = Lang.bind(this, this._menuRemoved);
 
-        Main.panel._menus.addMenu = Lang.bind(this, function(menu, position){
-            this.originalAddMenuFunc.apply(Main.panel._menus,[menu, position]);
-            this._menuAdded(menu);
-        });
-
-        //Add a hook on the removeMenu so that we can disconnect from the open-state-changed event.
-        this.originalRemoveMenuFunc = Main.panel._menus.removeMenu;
-
-        Main.panel._menus.removeMenu = Lang.bind(this, function(menu){
-            this.originalRemoveMenuFunc.apply(Main.panel._menus,[menu]);
-            this._menuRemoved(menu);
-        });
+        this._menuHook = new MenuHook(menuAddedCallback, menuRemovedCallback);
 
         this._menuData = new Array;
 
@@ -221,7 +269,8 @@ PanelVisibilityManager.prototype = {
     },
     setState: function(visibilityState){
         this.visibilityState = visibilityState;
-        this._settings.save({visibilityState:this.visibilityState});
+        this._settings.settings.visibilityState = this.visibilityState; 
+        this._settings.save();
 
         if(this._visibilityStateImpl){
             this._visibilityStateImpl.destroy();
@@ -283,13 +332,70 @@ PanelVisibilityManager.prototype = {
         Main.overview.disconnect(this._overviewHidingEventId);
         Main.overview.disconnect(this._overviewShowingEventId);
 
-        Main.panel._menus.addMenu = this.originalAddMenuFunc;
-        Main.panel._menus.removeMenu = this.originalRemoveMenuFunc;
-
         for(let menuDataIndex in this._menuData){
             let menuData = this._menuData[menuDataIndex];
             menuData.menu.disconnect(menuData.eventId);
         }
+
+        this._menuHook.destroy();
+    }
+}
+
+/* Panel Layout Manager */
+function PanelLayoutManager(settings){
+    this._init(settings);
+}
+
+PanelLayoutManager.prototype = {
+    _init: function(settings){
+        this._settings = settings;
+
+        let menuAddedCallback = Lang.bind(this, this._menuAdded);
+        let menuRemovedCallback = Lang.bind(this, this._menuRemoved);         
+
+        this._menuHook = new MenuHook(menuAddedCallback, menuRemovedCallback);
+
+        if(this._settings.settings.layoutState != undefined){
+            this.setState(this._settings.settings.layoutState);
+        }else{
+            this.setState(LAYOUT_TOP);
+        }
+    },
+    setState: function(state, save){
+        this.layoutState = state;
+
+        if(save){
+            this._settings.settings.layoutState = this.layoutState; 
+            this._settings.save();
+        }
+
+        switch(this.layoutState){
+            case LAYOUT_TOP:
+                Main.panel.actor.get_parent().set_y(0);
+                this._arrowSide = 0;
+                break;
+            case LAYOUT_BOTTOM:
+                Main.panel.actor.get_parent().set_y(Main.layoutManager.primaryMonitor.height - Main.panel.actor.get_height());
+                this._arrowSide = 2;
+                break;
+        }
+
+        this._updateMenuArrowSide();
+    },
+    _updateMenuArrowSide: function(){
+        for(let menuIndex in Main.panel._menus._menus){
+            let menu = Main.panel._menus._menus[menuIndex].menu;
+            this._menuAdded(menu);
+        }
+    },
+    _menuAdded: function(menu){
+        menu._boxPointer._arrowSide = this._arrowSide;
+    },
+    _menuRemoved: function(menu){
+    },
+    destroy: function(){
+        this.setState(LAYOUT_TOP, false);
+        this._menuHook.destroy();
     }
 }
 
@@ -305,23 +411,49 @@ PanelSettingsButton.prototype = {
         PanelMenu.SystemStatusButton.prototype._init.call(this, 'system-run');
 
         this._settings = new SettingsManager;
+        this._settings.load();
+
         this._visibilityManager = new PanelVisibilityManager(this._settings);
+        this._layoutManager = new PanelLayoutManager(this._settings);
         
+        this._createVisibilityMenu();
+        this._createLayoutMenu();
+    },
+    _createVisibilityMenu: function(){
         this._visibilityItems = new Array;
+
+        this._panelVisibilitySubMenu = new PopupMenu.PopupSubMenuMenuItem("Visibility");
+        this.menu.addMenuItem(this._panelVisibilitySubMenu);
     
         this._visibilityItems[VISIBILITY_NORMAL] = new PopupMenu.PopupMenuItem("Normal");
         this._visibilityItems[VISIBILITY_NORMAL].connect('activate', Lang.bind(this, this._onVisibilityNormalItemClick));
-        this.menu.addMenuItem(this._visibilityItems[VISIBILITY_NORMAL]);
+        this._panelVisibilitySubMenu.menu.addMenuItem(this._visibilityItems[VISIBILITY_NORMAL]);
         
         this._visibilityItems[VISIBILITY_AUTOHIDE] = new PopupMenu.PopupMenuItem("Autohide");
         this._visibilityItems[VISIBILITY_AUTOHIDE].connect('activate', Lang.bind(this, this._onVisibilityAutohideItemClick));
-        this.menu.addMenuItem(this._visibilityItems[VISIBILITY_AUTOHIDE]);
+        this._panelVisibilitySubMenu.menu.addMenuItem(this._visibilityItems[VISIBILITY_AUTOHIDE]);
 
         this._visibilityItems[VISIBILITY_OVERVIEW_ONLY] = new PopupMenu.PopupMenuItem("Overview Only");
         this._visibilityItems[VISIBILITY_OVERVIEW_ONLY].connect('activate', Lang.bind(this, this._onVisibilityOverviewOnlyItemClick));        
-        this.menu.addMenuItem(this._visibilityItems[VISIBILITY_OVERVIEW_ONLY]);
+        this._panelVisibilitySubMenu.menu.addMenuItem(this._visibilityItems[VISIBILITY_OVERVIEW_ONLY]);
         
         this._updateVisibilityState();
+    },
+    _createLayoutMenu: function(){
+        this._panelLayoutSubMenu = new PopupMenu.PopupSubMenuMenuItem("Layout");
+        this.menu.addMenuItem(this._panelLayoutSubMenu);
+
+        this._layoutItems = new Array;
+
+        this._layoutItems[LAYOUT_TOP] = new PopupMenu.PopupMenuItem("Top");
+        this._layoutItems[LAYOUT_TOP].connect('activate', Lang.bind(this, this._onLayoutTopItemClick));
+        this._panelLayoutSubMenu.menu.addMenuItem(this._layoutItems[LAYOUT_TOP]);
+
+        this._layoutItems[LAYOUT_BOTTOM] = new PopupMenu.PopupMenuItem("Bottom");
+        this._layoutItems[LAYOUT_BOTTOM].connect('activate', Lang.bind(this, this._onLayoutBottomItemClick));
+        this._panelLayoutSubMenu.menu.addMenuItem(this._layoutItems[LAYOUT_BOTTOM]);
+
+        this._updateLayoutState();
     },
     _onVisibilityOverviewOnlyItemClick: function(){
         this._visibilityManager.setState(VISIBILITY_OVERVIEW_ONLY);
@@ -335,6 +467,14 @@ PanelSettingsButton.prototype = {
         this._visibilityManager.setState(VISIBILITY_NORMAL);
         this._updateVisibilityState();
     },
+    _onLayoutTopItemClick: function(){
+        this._layoutManager.setState(LAYOUT_TOP, true);
+        this._updateLayoutState();
+    },
+    _onLayoutBottomItemClick: function(){
+        this._layoutManager.setState(LAYOUT_BOTTOM, true);
+        this._updateLayoutState();
+    },
     _updateVisibilityState: function(){
         for(let index in this._visibilityItems){
             this._visibilityItems[index].setShowDot(false);
@@ -342,8 +482,17 @@ PanelSettingsButton.prototype = {
         
         this._visibilityItems[this._visibilityManager.visibilityState].setShowDot(true);
     },
-    _destroy: function(){
+    _updateLayoutState: function(){
+        for(let index in this._layoutItems){
+            this._layoutItems[index].setShowDot(false);
+        }
+        
+        this._layoutItems[this._layoutManager.layoutState].setShowDot(true);
+    },
+    destroy: function(){
         this._visibilityManager.destroy();
+        this._layoutManager.destroy();
+        PanelMenu.SystemStatusButton.prototype.destroy.call(this);
     }
 };
 
@@ -359,6 +508,6 @@ function enable() {
 }
 
 function disable() {
-    _indicator._destroy();
     _indicator.destroy();
+    _indicator = null;
 }
